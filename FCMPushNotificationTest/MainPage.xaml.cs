289e459516e683ef96d5cb1f0595538ed6af09ba
abroodie <abroodie@gmail.com>
2023-11-30 21:14:52 +0000

    Add project files.

---
+++ FCMPushNotificationTest/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FCMPushNotificationTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
